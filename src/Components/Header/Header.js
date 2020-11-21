11aad418b6fc0e79362f527f203b4613eca481b1
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-11-21 16:18:16 -0700

    header-search

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,12 @@ import {connect} from 'react-redux'
 import {getUser} from '../../redux/reducer'
 import axios from 'axios'
 
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faSearch} from '@fortawesome/free-solid-svg-icons'
+import './header.css'
+
 function Header(props){
+    const [toggle, setToggle] = useState(true)
 
     const logoutUser = (t) => {
         axios.get('/api/logout')
@@ -20,16 +25,24 @@ function Header(props){
                 props.getUser(res.data)
             }).catch(err => console.log(err))
         }
-        console.log(props.user)
     }, [])
 
     return (
-        <div className='header-component'>
-            <button>Search</button>
-            {Object.keys(props.user).length !== 0 ? <p>Logged In as {props.user.first_name}</p> : <Link to='/account/login'>Log In</Link>}
-            {Object.keys(props.user).length !== 0 ? <Link to='/' onClick={logoutUser}>Log out</Link> :
-            <Link to='/account/register' >Create Account</Link>}
-            <Link to='/cart'>Cart</Link>
+        <div className={toggle === true ? 'header-component' : 'header-search-comp'}>
+            <input className={toggle === true ? 'none-header-search' : 'header-search-box'} />
+            <nav className='align-main'>
+                <div className='left-section' onClick={() => setToggle(!toggle)}>
+                <FontAwesomeIcon className='search-icon' icon={faSearch}></FontAwesomeIcon>
+                <button className='left-section-button'>Search</button> 
+                </div>
+                <nav className='right-section'>
+                    {Object.keys(props.user).length !== 0 ? <p className='user'>Logged In as {props.user.first_name}</p> : <Link className='link' to='/account/login'>Log In</Link>}
+                    <div className='login-border'></div>
+                    {Object.keys(props.user).length !== 0 ? <Link className='link' to='/' onClick={logoutUser}>Log out</Link> :
+                    <Link className='link' to='/account/register'>Create Account</Link>}
+                    <Link className='link' to='/cart'>Cart</Link>
+                </nav>
+            </nav>
         </div>
     )
 }

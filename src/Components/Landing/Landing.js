ff3d4a503dc38d7dca7e60f037c6c85c82d275db
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-12-02 22:59:51 -0700

    landing types

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -8,6 +8,20 @@ function Landing(props){
     return (
         <div className='landing-component'>
             <MyCarousel />
+            <div className='landing-main'>
+                <div className='landing-type'>
+                    <nav className='landing-main-type'>
+                        <div className='landing-shade-box'></div>
+                        <img src={'https://cdn.shopify.com/s/files/1/0966/8778/t/3/assets/homepage_featured_box_2_image.jpg?v=7528980631596753729'} alt='polarized glasses'/>
+                        <p>CLASSIC Matte</p>
+                    </nav>
+                    <nav className='landing-main-type'>
+                        <div className='landing-shade-box' ></div>
+                        <img src={'https://cdn.shopify.com/s/files/1/0966/8778/t/3/assets/homepage_featured_box_1_image.jpg?v=12104791585993755325'} />
+                        <p>Classic polarized</p>
+                    </nav>
+                </div>
+            </div>
         </div>
     )
 }

6c756d2634deafea3d7d2aa3c8aff6f95951f8b3
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-12-04 23:21:23 -0700

    login/register next

--- src/Components/Story/Story.js
+++ src/Components/Story/Story.js
@@ -30,6 +30,11 @@ function Story(props){
                 <hr></hr>
             </nav>
             <StoryVideo />
+            <div className='story-text-info'>
+                <p>We are a collective of dropouts and maybe even a scholar or two. With over 30 years experience designing, developing and creating products for some of the worlds top brands, we know what we like and expect out of the goods we use and abuse on the daily. We have been fortunate enough to have traveled the world and experience countless locations, both good and bad. Throughout our adventures we have learned that it's important to make the most of every opportunity, put Family, Friends and Yourself first, help others whenever you can and always have fun. LMWL was an idea and concept we have been chewing on for many years and we are excited to bring it to life in the form of a product we all love and use every day. Sunglasses featuring handcrafted, custom designs of a timeless and classic style. High-end quality and materials at a price that won't break your bank.</p>
+                <p>- - - - -</p>
+                <p>The LIVE MORE WORK LESS lifestyle is not about being lazy and not working hard - it is about making the most of your time. In these busy times, it is easy to get trapped in the meaningless cycle of Work, Eat, Sleep, Repeat. We believe there is so much more to life and that no good ideas are ever found behind a desk. We believe in taking advantage of every waking moment. We believe you should do what you love and spend the time doing it with the people you care about the most. Simplifying your life, so you can have more time to explore, discover, learn and have fun. Time is free, yet priceless and once it's gone - you can never get it back. <b>LIVE MORE WORK LESS.</b></p>
+            </div>
         </div>
     )
 }

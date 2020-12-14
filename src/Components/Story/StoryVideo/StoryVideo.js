29241ea19d0e4130b6dc25130ead5fa382ec347c
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-12-14 16:39:34 -0700

    more responsive styling

--- src/Components/Story/StoryVideo/StoryVideo.js
+++ src/Components/Story/StoryVideo/StoryVideo.js
@@ -8,9 +8,10 @@ function StoryVideo(){
     return (
         <div className='story-video-component'>
             <ReactPlayer 
+            className='react-player'
             url='https://player.vimeo.com/video/145534891?autoplay=1&color=6ecbb8'
             width={'100%'}
-            height={'63vh'} 
+            height={'100%'}
              />
         </div>
     )

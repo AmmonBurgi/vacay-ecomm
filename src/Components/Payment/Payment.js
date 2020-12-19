7364e8933f815dc3db5bb3e220c5a50fee91c69c
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-12-19 13:09:15 -0700

    payment inputs

--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -12,7 +12,27 @@ import {faDotCircle} from '@fortawesome/free-solid-svg-icons'
 function Payment(props){
     const [total, setTotal] = useState(0),
         [newBillingToggle, setNewBillingToggle] = useState(false),
-        [billingToggle, setBillingToggle] = useState(true)
+        [billingToggle, setBillingToggle] = useState(true),
+        [firstBill, setFirstBill] = useState(''),
+        [lastBill, setLastBill] = useState(''),
+        [companyBill, setCompanyBill] = useState(''),
+        [addressBill, setAddressBill] = useState(''),
+        [apartmentBill, setApartmentBill] = useState(''),
+        [cityBill, setCityBill] = useState(''),
+        [countryBill, setCountryBill] = useState(''),
+        [stateBill, setStateBill] = useState(''),
+        [zipBill, setZipBill] = useState(''),
+        [phoneBill, setPhoneBill] = useState(''),
+        [firstAlert, setFirstAlert] = useState(false),
+        [lastAlert, setLastAlert] = useState(false),
+        [addressAlert, setAddressAlert] = useState(false),
+        [cityAlert, setCityAlert] = useState(false),
+        [countryAlert, setCountryAlert] = useState(false),
+        [stateAlert, setStateAlert] = useState(false),
+        [zipAlert, setZipAlert] = useState(false),
+        [mexicoToggle, setMexico] = useState(false),
+        [canadaToggle, setCanada] = useState(false),
+        [unitedToggle, setUnited] = useState(true)
 
     const stripe = useStripe(),
         elements = useElements()
@@ -58,6 +78,51 @@ function Payment(props){
         setBillingToggle(!billingToggle)
         setNewBillingToggle(!newBillingToggle)
     }
+    const handleFirstInput = (e) => {
+        setFirstAlert(false)
+        setFirstBill(e.target.value)
+    }
+    const handleLastInput = (e) => {
+        setLastAlert(false)
+        setLastBill(e.target.value)
+    }
+    const handleAddressInput = (e) => {
+        setAddressAlert(false)
+        setAddressBill(e.target.value)
+    }
+    const handleCityInput = (e) => {
+        setCityAlert(false)
+        setCityBill(e.target.value)
+    }
+    const handleCountryInput = (event) => {
+        setCountryAlert(false)
+        setCountryBill(event.target.value)
+        if(event.target.value === 'US'){
+                setUnited(true)
+                setMexico(false)
+                setCanada(false)
+        }
+        if(event.target.value === 'CA'){
+            setUnited(false)
+            setMexico(false)
+            setCanada(true)
+        }
+        if(event.target.value === 'MX'){
+            setUnited(false)
+            setMexico(true)
+            setCanada(false)
+        }
+    }
+    const handleStateInput = (e) => {
+        setStateAlert(false)
+        setStateBill(e.target.value)
+    }
+    const handleZipInput = (e) => {
+        setZipAlert(false)
+        setZipBill(e.target.value)
+    }
+
+
     return(
         <div className='pay-component'>
             {Object.keys(props.purchaseState.purchaseInfo).length === 0 ? <AlertWarning warning={`You're missing information that is needed to finish your purchase! Please finish filling out your information `} pushFunction={() => props.history.push('/checkout/info')} /> : null}
@@ -173,7 +238,69 @@ function Payment(props){
                                 <p>Use a different billing address</p>
                             </nav>
                             <div className={newBillingToggle === true ? 'billing-input-wrapper' : 'none'}>
-                                  <p>hello</p>
+                                  <nav className='pay-align-first-last'>
+                                    <div
+                                    className={firstBill.length === 0 ? (firstAlert === false ? 'pay-box-input' : 'alert-pay-box') : (firstAlert === false ? 'pay-box-input-init' : 'alert-pay-box-input-init')}
+                                    >
+                                        <p>First Name</p>
+                                        <input 
+                                        onChange={(e) => handleFirstInput(e)} placeholder='First Name' />
+                                    </div>
+                                    <div>
+                                        <p>Last Name</p>
+                                        <input 
+                                        onChange={(e) => handleLastInput(e)}
+                                        placeholder='Last Name' />
+                                    </div>
+                                  </nav>
+                                  <div>
+                                      <p>Company (optional)</p>
+                                      <input
+                                      onChange={(e) => setCompanyBill(e.target.value)}
+                                      placeholder='Company (optional)' />
+                                  </div>
+                                  <div>
+                                      <p>Address</p>
+                                      <input 
+                                      onChange={(e) => handleAddressInput(e)}
+                                      placeholder='Address' />
+                                  </div>
+                                  <div>
+                                      <p>Apartment, suite, etc. (optional)</p>
+                                      <input
+                                      onChange={(e) => setApartmentBill(e.target.value)}
+                                      placeholder='Apartment, suite, etc.' />
+                                  </div>
+                                  <div>
+                                      <p>City</p>
+                                      <input
+                                      onChange={(e) => handleCityInput(e)}
+                                      placeholder='City' />
+                                  </div>
+                                  <nav>
+                                      <div>
+                                          <p>Country/Region</p>
+                                          <select onChange={(e) => handleCountryInput(e)}>
+
+                                          </select>
+                                      </div>
+                                      <div>
+                                          <p>State</p>
+                                          <select onChange={(e) => handleCityInput(e)}>
+
+                                          </select>
+                                      </div>
+                                      <div>
+                                          <p>Zip Code</p>
+                                          <input
+                                          onChange={(e) => handleZipInput(e)}
+                                          placeholder='Zip Code' />
+                                      </div>
+                                  </nav>
+                                  <div>
+                                      <p>Phone (optional)</p>
+                                      <input placeholder='Phone (optional)' />
+                                  </div>
                             </div>
                         </div>
                     </div>

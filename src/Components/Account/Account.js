2ad8b58a37aa76251394aca450090c312ba5b262
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2020-12-28 01:43:09 -0700

    account styling

--- src/Components/Account/Account.js
+++ src/Components/Account/Account.js
@@ -1,6 +1,11 @@
 import React, {useEffect, useState} from 'react'
 import {connect} from 'react-redux' 
 import axios from 'axios'
+import './account.css'
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faInstagram} from '@fortawesome/free-brands-svg-icons'
+import {faPinterest} from '@fortawesome/free-brands-svg-icons'
 
 function Account(props){
     const [products, setProducts] = useState([]),
@@ -15,16 +20,18 @@ function Account(props){
     }, [])
 
     const productMap = products.map((element, index) => {
+        console.log(element)
         return (
             <div key={index} >
-                <p>{element.date}</p>
+                <p>{element.product_title}</p>
             </div>
         )
     })
     const historyMap = history.map((element, index) => {
+        console.log(element)
         return (
             <div key={index} >
-                <p>{element.product_title}</p>
+                <p>{element.date_purchased}</p>
             </div>
         )
     })
@@ -32,13 +39,39 @@ function Account(props){
     console.log(products)
     console.log(history)
     return (
-        <div>
-            <div>
-                {productMap}
+        <div className='account-component'>
+           <div className='account-prev'>
+                <nav className='account-prev-left'>
+                    <p className='account-prev-home' onClick={() => props.history.push('/')}>Home </p>
+                    <p className='account-prev-arrow'>&#62;</p>
+                    <p className='account-prev-arrow'>My account</p>
+                </nav>
+                <nav className='account-icons'>
+                    <a href='https://www.instagram.com/livemoreworkless/' >
+                        <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+                    </a>
+                    <a href='https://www.pinterest.com/vacaysunglasses/' >
+                        <FontAwesomeIcon icon={faPinterest}></FontAwesomeIcon>
+                    </a>
+                </nav>
             </div>
-            <div>
-                {historyMap}
+            {Object.keys(props.authState.user).length === 0 ? 
+            (
+            <div className='no-account-component'>
+                <div className='my-account-tag'>
+                    <p>My Account</p>
+                    <hr></hr>
+                </div>
+                <p className='no-account-text'>Please Login <b onClick={() => props.history.push('/account/login')}>here</b> to access your account!</p>
             </div>
+            )
+            : 
+            (
+            <div className='account-main'>
+                <p>Order History</p>
+                {}
+            </div>
+            )}
         </div>
     )
 }

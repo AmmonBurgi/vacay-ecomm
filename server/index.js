ae0ac5ea974a19059aea1a4baf82c6e50b80ed13
Ammon Burgi <ammonburgi@Ammons-MacBook-Pro.local>
2021-05-10 17:00:45 -0600

    express.static

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ const express = require('express'),
     const path = require('path')
 
     app.use(express.json())
+    app.use(express.static(__dirname + '/../build'))
 
     app.use(session({
         resave: false,
